d6b9402e1787e8ae54ffbb57575a85f6db51472c
Aneesh <aneeshappid@gmail.com>
2025-06-26 23:22:03 +0530

    2311

---
+++ Daily/LongestBinarySequenceLessThanK.py
@@ -0,0 +1,21 @@
+
+class Solution:
+    def longestSubsequence(self, s: str, k: int) -> int:
+        
+        n = len(s)
+        zeros = s.count('0')
+        ones = 0
+        value = 0
+        power = 1
+
+        for i in range(n-1, -1, -1):
+            if s[i] == '1':
+                if value + power > k:
+                    continue
+                ones += 1
+                value += power
+            power = power << 1
+            if power > k:
+                break
+        return zeros + ones
+  
