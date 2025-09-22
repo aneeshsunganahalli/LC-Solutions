c14abee54921abf0b9b42971c0a0ffc218a25915
Aneesh <aneeshappid@gmail.com>
2025-09-22 14:04:23 +0530

    15

--- TwoPointers/3Sum.py
+++ TwoPointers/3Sum.py
@@ -1,33 +1,61 @@
 from typing import List
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
 
-        res = []
         nums.sort()
+        res = []
 
         for i, a in enumerate(nums):
-
-            # Without negative number, we can't form a triplet, (a+b+c)
-            if a > 0:
+            if a > 0:  # Needs to be non-positive for sum of zero
                 break
-            
-            if i > 0 and a == nums[i - 1]: # Don't want to repeat values for a
+            if i > 0 and a == nums[i - 1]:  # Already processed
                 continue
-            
-            l = i + 1
-            r = len(nums) - 1
-            while l < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
-                    l += 1
+
+            j = i + 1
+            k = len(nums) - 1
+
+            while j < k:
+                threeSum = nums[j] + nums[k] + a
+                if threeSum < 0:
+                    j += 1
+                elif threeSum > 0:
+                    k -= 1
                 else:
-                    res.append([a,nums[l],nums[r]])
-                    l += 1
-                    r -= 1
-                    while nums[l] == nums[l - 1] and l < r:
-                        l += 1
+                    res.append([a, nums[j], nums[k]])
+                    j += 1
+                    k -= 1 # To avoid duplicate, if we use same j,k we just get the same triplet later
+                    while nums[j] == nums[j - 1] and j < k:
+                        j += 1
         return res
-        
+
+        # Quadratic
+        ansSet = set()
+        for i in range(len(nums)):
+            tri = set()
+            for j in range(i + 1, len(nums)):
+                third = -(nums[i] + nums[j])
+                if third in tri:
+                    temp = [nums[i], nums[j], third]
+                    temp.sort()
+                    ansSet.add(tuple(temp))
+                tri.add(nums[j])
+
+        return list(ansSet)
+
+        # Brute Force (TLE)
+        nums.sort()
+        print(nums)
+        a = 0
+        res = set()
+
+        while a < len(nums) - 2:
+            b = a + 1
+            while b < len(nums) - 1:
+                c = b + 1
+                while c < len(nums):
+                    if nums[a] + nums[b] + nums[c] == 0:
+                        res.add((nums[a], nums[b], nums[c]))
+                    c += 1
+                b += 1
+            a += 1
+        return list(res)
